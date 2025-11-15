8fb0c12dad61f6281e40f29a23267ccf45b1e3ec
Esteban Vega <esteban.veg4@gmail.com>
2025-11-15 22:22:17 +0100

    Enhance issue fetching functionality by integrating OpenAI and updating dependencies in package.json and package-lock.json

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 import { Octokit } from "octokit";
-
-console.info("Fetching issues from seb-oss/green...");
+import OpenAI from "openai";
+import { encode } from '@toon-format/toon';
+import { marked } from 'marked';
+import { markedTerminal } from 'marked-terminal';
 
 const octokit = new Octokit({
     auth: process.env.GH_ACCESS_TOKEN
+});
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 async function fetchIssues(limit) {
@@ -26,5 +31,62 @@ async function fetchIssues(limit) {
     return issues;
 }
 
-const issues = await fetchIssues(10);
-console.info(`Fetched ${issues.length} issues.`);
+async function processIssues(issues) {
+    const encodedData = encode(issues);
+
+    const response = await openai.responses.create({
+        prompt: {
+            "id": "pmpt_6918e75dee9081959ec957cfc814099805b4b81d337e32f7",
+            "version": "2",
+            "variables": {
+                "similar_issues": encodedData,
+                "title": "Tooltip: Renders behind dialog",
+                "body": '### Bug already reported?\n' +
+                    '\n' +
+                    '- [x] I confirm that I have checked if the bug already has been reported\n' +
+                    '\n' +
+                    '### For which framework/library you are reporting the bug\n' +
+                    '\n' +
+                    'Angular\n' +
+                    '\n' +
+                    '### Component name\n' +
+                    '\n' +
+                    'Tooltip\n' +
+                    '\n' +
+                    '### Description\n' +
+                    '\n' +
+                    'Currently if you want to use tooltips in a green dialog (slide-out variant) tooltip is added to the DOM, but its hidden under the dialog. and is not visible\n' +
+                    '\n' +
+                    '### Steps To Reproduce\n' +
+                    '\n' +
+                    '1. Login into nibp\n' +
+                    '2. Navigate "Pay and transfer" -> "My recipients"\n' +
+                    '3. Click on any existing recipient that has either long recipient name or bank name (is truncated)\n' +
+                    '4. Once dialog opens, hover over truncated text\n' +
+                    '\n' +
+                    'Notice that tooltip is not being visible, but in DOM new element is attached to body and removed once mouse is not hovering truncated text\n' +
+                    '\n' +
+                    '### Current Behaviour\n' +
+                    '\n' +
+                    'Tooltip element is added to DOM but is hidden behind the dialog\n' +
+                    '\n' +
+                    '### Expected Behaviour\n' +
+                    '\n' +
+                    'Tooltip element is added to DOM and is visible in dialog',
+            }
+        }
+    });
+
+    return response;
+}
+
+async function run() {
+    const issues = await fetchIssues(50);
+    const result = await processIssues(issues);
+    console.info("Result: ", result.output_text);
+
+    marked.use(markedTerminal());
+    console.info(await marked.parse(result.output_text));
+}
+
+run();
